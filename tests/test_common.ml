4efeb76a09046a70529e9f8a049f7cb88a0e08c7
Jon Ludlam <jonathan.ludlam@citrix.com>
2015-08-27 11:24:32 +0100

    Use the new sm-config allocation value (xlvhd rather than dynamic)
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- tests/test_common.ml
+++ tests/test_common.ml
@@ -175,7 +175,7 @@ let create_iscsi_sr state =
     ~device_config:["target", iscsi.iscsi_ip; "targetIQN", iscsi.iqn; "SCSIid", scsiid]
     ~_type:"lvmoiscsi" ~physical_size:0L ~name_label:"xenvm"
     ~name_description:"" ~content_type:""
-    ~sm_config:["allocation","dynamic"] ~shared:true
+    ~sm_config:["allocation","xlvhd"] ~shared:true
 
   
 let get_iscsi_sr state =
